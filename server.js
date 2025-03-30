d0214f59ab7ed072c65debafb37c7ceba60ad9c0
Mingzhi <mingzhi@LAPTOP-BIAL5JFG.>
2025-03-29 22:41:28 -0400

    second commit

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const app = express();
 
 app.use(express.static('frontend'));
 
+// ignore this function
 app.get('/run', (req, res) => {
-    exec('python3 parse_tree.py', { cwd: 'backend' },  (error, stdout, stderr) => {
+    exec('python3 backend/parse_tree.py', (error, stdout, stderr) => {
       if (error) return res.status(500).send(stderr);
       try {
         const json = JSON.parse(stdout);
         res.json(json);
-        console.log("json")
       } catch (err) {
         res.status(500).send("Invalid JSON from Python");
       }
     });
   });
+
+
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
